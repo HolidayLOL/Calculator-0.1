5ee7c148aadf6d9ba330dfd2274330cb566018ff
Holiday <102799984+HolidayLOL@users.noreply.github.com>
2022-09-23 17:23:31 +0300

    Fix input error (#2)

--- src/Holiday/Calculator.java
+++ src/Holiday/Calculator.java
@@ -3,43 +3,82 @@ package Holiday;
 import java.util.Scanner;
 
 public class Calculator {
-    public static Integer calculate(Scanner input) {
+
+    private static final String ACTION_PLUS = "+";
+    private static final String ACTION_MINUS = "-";
+    private static final String ACTION_DIVIDE = "/";
+    private static final String ACTION_MULTIPLY = "*";
+    private static final String MESSAGE_ENTER_FIRST_NUMBER = "Enter first number: ";
+    private static final String MESSAGE_ENTER_SECOND_NUMBER = "Enter second number: ";
+    private static final String MESSAGE_ENTER_ACTION = "Enter action [+ - / *]: ";
+    private static final String MESSAGE_RESULT = "Result: ";
+    private static final String MESSAGE_ERROR = "You entered something wrong! :( ";
+
+
+    public static void calculate(Scanner input) {
         while (true) {
-            System.out.println("Enter first number: ");
-            int num1 = input.nextInt();
-
             int res;
-
-            System.out.println("Enter two number: ");
-            int num2 = input.nextInt();
-
-            System.out.println("Enter action [+ - / *]: ");
-            String action = input.nextLine();
-            action = input.nextLine();
-
+            int num1 = getNumber(input, MESSAGE_ENTER_FIRST_NUMBER);
+            int num2 = getNumber(input, MESSAGE_ENTER_SECOND_NUMBER);
+            String action;
+            action = getAction(input, MESSAGE_ENTER_ACTION);
             switch (action) {
-                case "+":
+                case ACTION_PLUS:
                     res = num1 + num2;
-                    System.out.println("Result: " + res);
-                    break;
-                case "-":
+                    printResult(MESSAGE_RESULT, res);
+                    return;
+                case ACTION_MINUS:
                     res = num1 - num2;
-                    System.out.println("Result: " + res);
-                    break;
-                case "*":
+                    printResult(MESSAGE_RESULT, res);
+                    return;
+                case ACTION_MULTIPLY:
                     res = num1 * num2;
-                    System.out.println("Result: " + res);
-                    break;
-                case "/":
+                    printResult(MESSAGE_RESULT, res);
+                    return;
+                case ACTION_DIVIDE:
                     res = num1 / num2;
-                    System.out.println("Result: " + res);
-                    break;
+                    printResult(MESSAGE_RESULT, res);
+                    return;
                 default:
-                    System.out.println("\n" +
-                            "You entered something wrong! :(");
-                    break;
+                    printResult("\n" + MESSAGE_ERROR);
+                    return;
             }
         }
     }
+
+    private static int getNumber(Scanner input, String message) {
+        printMessage(message);
+        try {
+            return Integer.parseInt(input.next());
+        } catch (NumberFormatException e) {
+            printMessage(MESSAGE_ERROR);
+            return getNumber(input, message);
+        }
+    }
+
+    private static String getAction(Scanner input, String message) {
+        printMessage(message);
+        String action = input.next();
+        if (action.equals(ACTION_PLUS) || action.equals(ACTION_MINUS) || action.equals(ACTION_MULTIPLY) ||
+            action.equals(ACTION_DIVIDE)) {
+            return action;
+        } else {
+            printMessage(MESSAGE_ERROR);
+            return getAction(input, message);
+        }
+    }
+
+    private static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private static void printResult(String message, int res) {
+        System.out.println(message + res);
+    }
+
+    private static void printResult(String message) {
+        System.out.println(message);
+    }
 }
+
 
